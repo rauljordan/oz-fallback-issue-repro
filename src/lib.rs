812a4eaf0eade6b1f1c944f4ad3528647ba7a6ae
Raul Jordan <raul@prysmaticlabs.com>
2024-12-23 09:51:49 -0600

    first

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+#![cfg_attr(not(feature = "export-abi"), no_main)]
+extern crate alloc;
+
+use stylus_sdk::{alloy_primitives::U256, prelude::*, storage::StorageU256};
+
+#[storage]
+pub struct Counter {
+    number: StorageU256,
+}
+
+#[public]
+impl Counter {
+    pub fn number(&self) -> U256 {
+        self.number.get()
+    }
+    pub fn set_number(&mut self, new_number: U256) {
+        self.number.set(new_number);
+    }
+    #[receive]
+    pub fn receive(&mut self) -> Result<(), Vec<u8>> {
+        Err(vec![1, 2, 3])
+    }
+    #[fallback]
+    pub fn fallback(&mut self, _calldata: &[u8]) -> Result<Vec<u8>, Vec<u8>> {
+        let some_vec = [5, 6, 7, 8].to_vec();
+        Ok(some_vec)
+    }
+}
